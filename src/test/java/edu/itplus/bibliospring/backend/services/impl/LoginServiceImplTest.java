e50d764674efa56447d02b2e447edbacf1ab231a
Ágoston Denezsi <agostondenezsi@mail.com>
2025-07-25 14:01:12 +0300

    Service mocking done.

--- src/test/java/edu/itplus/bibliospring/backend/services/impl/LoginServiceImplTest.java
+++ src/test/java/edu/itplus/bibliospring/backend/services/impl/LoginServiceImplTest.java
@@ -1,33 +1,57 @@
 package edu.itplus.bibliospring.backend.services.impl;
 
+import edu.itplus.bibliospring.backend.model.User;
+import edu.itplus.bibliospring.backend.repository.UserDAO;
+import edu.itplus.bibliospring.backend.utils.PasswordEncrypter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 class LoginServiceImplTest {
     private LoginServiceImpl serviceUnderTest;
 
-    private TestUserDAO testUserDao;
+    private UserDAO testUserDao;
+
+    private PasswordEncrypter testPasswordEncrypter;
+
+    private User nonDbUser;
+
+    private User dbUser;
 
     @BeforeEach
     void setUp() {
         // Initialize the service under test
         serviceUnderTest = new LoginServiceImpl();
 
-        testUserDao = new TestUserDAO();
+        nonDbUser = new User();
+        nonDbUser.setPassword(TestPasswordEncrypter.password);
+        nonDbUser.setUsername("Pistike");
+        nonDbUser.setUuid(TestPasswordEncrypter.salt);
+        nonDbUser.setId(1L);
+
+        dbUser = new User();
+        dbUser.setPassword(TestPasswordEncrypter.hashedPassword);
+        dbUser.setUsername("Pistike");
+        dbUser.setUuid(TestPasswordEncrypter.salt);
+        dbUser.setId(1L);
+
+        testUserDao = mock(UserDAO.class);
+        testPasswordEncrypter = mock(PasswordEncrypter.class);
+        when(testPasswordEncrypter.hashPassword(nonDbUser.getPassword(), nonDbUser.getUuid()))
+                .thenReturn(dbUser.getPassword());
 
         ReflectionTestUtils.setField(serviceUnderTest, "userDAO", testUserDao);
-        ReflectionTestUtils.setField(serviceUnderTest, "passwordEncrypter", new TestPasswordEncrypter());
+        when(testUserDao.findByUsername(nonDbUser.getUsername())).thenReturn(dbUser);
+
+        ReflectionTestUtils.setField(serviceUnderTest, "passwordEncrypter", testPasswordEncrypter);
 
         // Mock dependencies if necessary, e.g., UserDAO and PasswordEncrypter
         // This can be done using a mocking framework like Mockito
         // For example:
         // userDAO = mock(UserDAO.class);
-        // passwordEncrypter = mock(PasswordEncrypter.class);
-        // serviceUnderTest.setUserDAO(userDAO);
-        // serviceUnderTest.setPasswordEncrypter(passwordEncrypter);
 
 
     }
@@ -39,14 +63,15 @@ class LoginServiceImplTest {
 
         // Act
         // Call the login method with the mock User object
-        boolean loginStatus = serviceUnderTest.login(TestUserDAO.nonDbUser);
+        boolean loginStatus = serviceUnderTest.login(nonDbUser);
 
         // Assert
-        // Verify that the returned value matches the expected result (true or false)
         // Verify that the UserDAO's findByUsername method was called with the correct username
         // Verify that the PasswordEncrypter's hashPassword method was called with the correct parameters
         assertThat(loginStatus).isTrue();
-        assertThat(testUserDao.getLastSearchedUsernName()).isEqualTo(TestUserDAO.nonDbUser.getUsername());
+        verify(testUserDao, times(1)).findByUsername(nonDbUser.getUsername());
+
+        //assertThat(testUserDao.getLastSearchedUsernName()).isEqualTo(TestUserDAO.nonDbUser.getUsername());
     }
 
     @Test
--- src/test/java/edu/itplus/bibliospring/backend/services/impl/TestPasswordEncrypter.java
+++ src/test/java/edu/itplus/bibliospring/backend/services/impl/TestPasswordEncrypter.java
@@ -2,16 +2,8 @@ package edu.itplus.bibliospring.backend.services.impl;
 
 import edu.itplus.bibliospring.backend.utils.PasswordEncrypter;
 
-public class TestPasswordEncrypter implements PasswordEncrypter {
+public class TestPasswordEncrypter {
     public static final String password = "123";
     public static final String salt = "salt";
     public static final String hashedPassword = "hashed123";
-
-    @Override
-    public String hashPassword(String password, String salt) {
-        if (password.equals(TestPasswordEncrypter.password)
-                && salt.equals(TestPasswordEncrypter.salt)) {
-            return hashedPassword;
-        } else throw new IllegalArgumentException("Invalid password or salt");
-    }
 }
