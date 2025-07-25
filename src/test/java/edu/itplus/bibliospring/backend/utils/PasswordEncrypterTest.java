e155f8adc68ec7f2b1effe3887b7e7e814e38cad
Ágoston Denezsi <agostondenezsi@mail.com>
2025-07-25 11:05:03 +0300

    unit testing for hashPassword successful

--- src/test/java/edu/itplus/bibliospring/backend/utils/PasswordEncrypterTest.java
+++ src/test/java/edu/itplus/bibliospring/backend/utils/PasswordEncrypterTest.java
@@ -9,15 +9,14 @@ class PasswordEncrypterTest {
     void hashPassword_passwordIsHashed_hashedPasswordIsCorrect() {
         // Arrange
         String password = "password123";
-        String salt = UUID.randomUUID().toString();
+        String salt = "18b09ff8-9718-49ea-9588-c1a65ff10d8e";
         PasswordEncrypter systemUnderTest = new PasswordEncrypter();
 
         // Act
         String hashedPassword = systemUnderTest.hashPassword(password, salt);
 
         // Assert
-        String expectedHash = "D0E4735D4B1595FA719032A1DC05A26B23CBCD876EDD6DB7A5F4496BF13488A5";
-
+        String expectedHash = "7DA449FC9ECCB29FF0312FFF1F88C125B38B2F1EE5C37DD52B52A1EE9593883B";
         assertThat(hashedPassword).isEqualTo(expectedHash);
     }
 }
