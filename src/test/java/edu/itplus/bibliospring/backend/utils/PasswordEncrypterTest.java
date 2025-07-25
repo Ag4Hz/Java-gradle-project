55fd74655605ae95e01d2058e92a028754a2bcf2
Ágoston Denezsi <agostondenezsi@mail.com>
2025-07-25 10:57:05 +0300

    unit testing started, first test failed

--- src/test/java/edu/itplus/bibliospring/backend/utils/PasswordEncrypterTest.java
+++ src/test/java/edu/itplus/bibliospring/backend/utils/PasswordEncrypterTest.java
@@ -1,7 +1,23 @@
 package edu.itplus.bibliospring.backend.utils;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import java.util.UUID;
+import static org.assertj.core.api.Assertions.*;
 
 class PasswordEncrypterTest {
+    @Test
+    void hashPassword_passwordIsHashed_hashedPasswordIsCorrect() {
+        // Arrange
+        String password = "password123";
+        String salt = UUID.randomUUID().toString();
+        PasswordEncrypter systemUnderTest = new PasswordEncrypter();
 
+        // Act
+        String hashedPassword = systemUnderTest.hashPassword(password, salt);
+
+        // Assert
+        String expectedHash = "D0E4735D4B1595FA719032A1DC05A26B23CBCD876EDD6DB7A5F4496BF13488A5";
+
+        assertThat(hashedPassword).isEqualTo(expectedHash);
+    }
 }
